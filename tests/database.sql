33703e5c3c4ed26672188ab8ea6b35ddb76328a4
Francois Chaumont <funcois@outlook.com>
2018-04-15 19:45:58 -0500

    Initial commit

---
+++ tests/database.sql
@@ -0,0 +1,50 @@
+# create user 
+CREATE USER 'udbclass'@'localhost' IDENTIFIED BY 'udbclasspassw';
+GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'udbclass'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+
+# create database
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `tests_dbclass` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `tests_dbclass`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+TRUNCATE TABLE `users`;
+INSERT INTO `users` (`id`, `firstname`, `lastname`) VALUES
+(1, 'Tom', 'Cruise'),
+(2, 'Angelina', 'Jolie'),
+(3, 'Megan', 'Fox'),
+(4, 'Donnie', 'Yen'),
+(5, 'Gal', 'Gadot');
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `users`
+  ADD `level` INT NOT NULL AFTER `lastname`;
+
+update users set level = 1 where id % 2 = 0;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tests/tests.php
@@ -0,0 +1,143 @@
+<?php
+    require __DIR__ . '/../src/db.php';
+
+    $dbname = 'tests_dbclass';
+    $host = 'localhost';
+    $login = 'udbclass';
+    $password = 'udbclasspassw';
+    
+// instantiation of the class and attempted connection to the database
+    echo '### Connection to the database: ';
+    $db = new Db($dbname, $host, $login, $password);
+
+    if ($db->isConnected()) { 
+        echo "<font color='green'>CONNECTED</font>".'<br><br>'; 
+        print_r_pre($db);
+    }
+    else {
+        echo "<font color='red'>NOT CONNECTED</font>".'<br>'; 
+        echo $db->getErrMessage();
+    }
+    
+    if ($db->isConnected()) {
+
+
+// select with syntax error
+        echo '### Select query with syntax error <br>';
+        $db->emptyParams();
+        $selectQuery = "select * from users_error";
+        $success = $db->select($selectQuery);
+        if (!$success) { echo 'ERROR: '.$db->getErrMessage().'<br><br>'; }
+        else { 
+            while ($row = $db->getNextRow()) {
+                print_r_pre($row);
+            }
+        }
+
+// select with no parameters and order by
+        echo '### Select query with no parameters and order by <br>';
+        $db->emptyParams();
+        $selectQuery = "select * from users order by id desc";
+        $db->select($selectQuery);
+        while ($row = $db->getNextRow()) {
+            echo 'id: '.$row['id'].' / firstname: '.$row['firstname'].' / lastname: '.$row['lastname'].'<br>';
+            print_r_pre($row);
+        }
+
+// select with parameters
+        echo '### Select query with parameters <br>';
+        $db->emptyParams();
+        $selectQuery = "select * from users where id = :id or id = :id5";
+        $db->addParamToBind('id', 1);
+        $db->addParamToBind('id5', 5);
+        $db->select($selectQuery);
+        while ($row = $db->getNextRow()) {
+            print_r_pre($row);
+        }
+
+// select with group by
+        echo '### Select query with group by <br>';
+        $db->emptyParams();
+        $selectQuery = "select level, count(*) totalPerLevel from users group by level";
+        $db->select($selectQuery);
+        while ($row = $db->getNextRow()) {
+            print_r_pre($row);
+        }
+
+// insert attempt with duplicated primary key
+        echo '### Insert query with duplicated primary key <br>';
+        $db->emptyParams();
+        $insertQuery = "insert into users (id, firstname, lastname) values (:id, :firstname, :lastname)";
+        $db->addParamToBind('id', 1);
+        $db->addParamToBind('firstname', 'Michael');
+        $db->addParamToBind('lastname', 'Jackson');
+        $success = $db->insert($insertQuery);
+
+        if (!$success) { echo 'ERROR: '.$db->getErrMessage().'<br><br>'; }
+
+// insert user Michael Jackson
+        echo '### Insert query with parameters <br>';
+        $db->emptyParams();
+        $insertQuery = "insert into users (firstname, lastname) values (:firstname, :lastname)";
+        $db->addParamToBind('firstname', 'Michael');
+        $db->addParamToBind('lastname', 'Jackson');
+        $db->insert($insertQuery);
+
+        $lastId = $db->getLastId();
+        echo 'Last inserted id = '.$lastId;
+        $db->emptyParams();
+        $selectQuery = "select * from users where id = :id";
+        $db->addParamToBind('id', $lastId);
+        if ($db->select($selectQuery)) {
+            if ($row = $db->getNextRow()) {
+                print_r_pre($row);
+            }
+        }
+
+// update last inserted record
+        echo "### Update the last inserted record (id=$lastId) modifying lastname 'Jackson' into 'Jordan' <br>";
+        $db->emptyParams();
+        $updateQuery = "update users set lastname = :lastname where id = :id";
+        $db->addParamToBind('id', $lastId);
+        $db->addParamToBind('lastname', 'Jordan');
+        $db->update($updateQuery);
+
+        $db->emptyParams();
+        $selectQuery = "select * from users where id = :id";
+        $db->addParamToBind('id', $lastId);
+        if ($db->select($selectQuery)) {
+            if ($row = $db->getNextRow()) {
+                print_r_pre($row);
+            }
+        }
+
+// delete last inserted record
+        echo "### Delete the last inserted record (id=$lastId) <br>";
+        $db->emptyParams();
+        $deleteQuery = "delete from users where id = :id";
+        $db->addParamToBind('id', $lastId);
+        $deleted = $db->delete($deleteQuery);
+        if ($deleted) { echo "Record deleted!<br>"; }
+
+        $db->emptyParams();
+        $selectQuery = "select * from users where id = :id";
+        $db->addParamToBind('id', $lastId);
+        if ($db->select($selectQuery)) {
+            if ($row = $db->getNextRow()) {
+                print_r_pre($row);
+            }
+            else { echo "Record id $lastId NOT found! <br><br>"; }
+        }
+    }
+
+
+
+    
+// FUNCTIONS
+    function print_r_pre($mixed = null) {
+        echo '<pre>';
+        print_r($mixed);
+        echo '</pre>';
+        return null;
+    }
+
